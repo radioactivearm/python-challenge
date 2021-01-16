a2d93ed30fcecfe9ef60c9ef29c968c79767b0e9
arm <andy.mcrae21@gmail.com>
2021-01-16 16:02:10 -0600

    added comments for readability and started on the printing section

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,10 @@
 
 # This script analyzes finacial records for my company
 
+#--------------------------------------------------------
 # import modules
 import os
 import csv
-
 
 #-----------------------------------------------------------
 # functions
@@ -31,7 +31,7 @@ def sum_profit(profit):
 
 
 #-----------------------------------------------------------
-
+# open and read file into a dictionary of lists
 
 # creating path to csv file
 csvpath = os.path.join('Resources','budget_data.csv')
@@ -39,14 +39,14 @@ csvpath = os.path.join('Resources','budget_data.csv')
 # opening csv file for reading
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+    # print(csvreader)
 
     # taking the header from csv file and using them
     # to create keys in a dictionary
     keys_comma = next(csvfile)
     keys = keys_comma.split(',')
     keys[1] = keys[1].replace('\n','')
-    print(keys)
+    # print(keys)
     
     # creating dictionary with headers as keys
     # keys[0] = 'Date' , keys[1] = 'Profit/Losses'
@@ -60,12 +60,25 @@ with open(csvpath) as csvfile:
         finance['Date'].append(row[0])
         finance['Profit/Losses'].append(row[1])
 
-    print(finance['Date'][-1])
-    print(len(finance['Date']))
+    # print(finance['Date'][-1])
+    # print(len(finance['Date']))
 
-# use total_months on dictionary
-print(total_months(finance['Date'],finance['Profit/Losses']))
+#----------------------------------------------------------------------
+# use functions on dictionary to find desired values (and save them)
 
-# use sum_profit on dictionary
-print(sum_profit(finance['Profit/Losses']))
+# # use total_months on dictionary
+tot_mon = (total_months(finance['Date'],finance['Profit/Losses']))
+
+# # use sum_profit on dictionary
+sum_prof = (sum_profit(finance['Profit/Losses']))
+
+#----------------------------------------------------------------------------
+
+# Print Out A nice summary of findings
+print('Financial Analysis')
+print('----------------------------')
+#printing a formating string of total months
+print(f'Total Months: {tot_mon}')
+# printing a formated string of total profits
+print(f'Total: ${sum_prof}')
     

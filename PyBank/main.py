3b864553c8136f4bf5c2df6d8af556dd0fc1dd46
arm <andy.mcrae21@gmail.com>
2021-01-16 15:39:37 -0600

    added two functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,28 +8,29 @@ import os
 import csv
 
 
+#-----------------------------------------------------------
+# functions
+
+# find total number of months
+def total_months(date,profit):
+    # pass in lists
+    totals = len(date)
+    return totals
+
+# Sums the profit/losses
+def sum_profit(profit):
+    # pass in list
+    total = 0
+    # goes thru list and sums the contents
+    # (turns them into integers since I can't assume
+    #  that they already are)
+    for i in profit:
+        total += int(i)
+    return(total)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#-----------------------------------------------------------
 
 
 # creating path to csv file
@@ -47,28 +48,6 @@ with open(csvpath) as csvfile:
     keys[1] = keys[1].replace('\n','')
     print(keys)
     
-    # ---------------------------------------------------------------------
-    # old way of creating dictionary. turns out it just wasn't working 
-    # because of a typo.
-
-    # # read rest of file into lists which will be put into dictonary
-    # date_list = []
-    # profit_list = []
-
-    # for row in csvreader:
-    #     date_list.append(row[0])
-    #     profit_list.append(row[1])
-
-    # # creating dictionary with headers as keys
-    # # keys[0] = 'Date' , keys[1] = 'Profit/Losses'
-    # finance = {}
-    # finance[keys[0]] = date_list
-    # finance[keys[1]] = profit_list
-    # # print(finance)
-
-    # print(finance['Date'][1])
-    # ---------------------------------------------------
-
     # creating dictionary with headers as keys
     # keys[0] = 'Date' , keys[1] = 'Profit/Losses'
     finance = {}
@@ -83,4 +62,10 @@ with open(csvpath) as csvfile:
 
     print(finance['Date'][-1])
     print(len(finance['Date']))
+
+# use total_months on dictionary
+print(total_months(finance['Date'],finance['Profit/Losses']))
+
+# use sum_profit on dictionary
+print(sum_profit(finance['Profit/Losses']))
     

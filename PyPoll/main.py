0c946d9ec5272184b62a4a4dc052e0d59687fe07
arm <andy.mcrae21@gmail.com>
2021-01-19 12:07:06 -0600

    changed from lists to dictionarys

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,7 +24,7 @@ def count_poll(vote):
 # building on this one, making it find number of votes as it finds candidates
 def candidates(candidates):
     
-    runner = []
+    runner = {}
     
     # i am thinking that there is something about how list comprehension works that is alluding me here
     # because this just does not work
@@ -34,33 +34,46 @@ def candidates(candidates):
     for candi in candidates:
         # if they are not already in list of unique candidates, this will add them
         if candi not in runner:
-            runner.append(candi)
-
+            runner[candi] = candi
 
     # returns list of candidates
     return runner
 
-# this function is going to calculate total
-def totaling_vote(votes,num_of_candi):
+# this function is going to calculate total returns a dictionary
+def totaling_vote(votes):
 
-    # creating a list of zeros equal to length of candidates; imported numpy for this :)
-    total_votes = np.zeros(len(num_of_candi))
+    dict_of_candi = {}
 
-    #loop thru all votes casted
     for vote in votes:
-        # loop thru all candidates
-        for candi in num_of_candi:
-            # and check which candidate the vote was cast for
-            if vote == candi:
-                # then tally that vote under the candidates slot
-                total_votes[num_of_candi.index(candi)] += 1
-    # returns a list that corresponds with candidate list
-    return total_votes
+        if vote not in dict_of_candi:
+            dict_of_candi[vote] = 1
+        else:
+            dict_of_candi[vote] += 1
+
+    return dict_of_candi
+    # # creating a list of zeros equal to length of candidates; imported numpy for this :)
+    # total_votes = np.zeros(len(num_of_candi))
+
+    # #loop thru all votes casted
+    # for vote in votes:
+    #     # loop thru all candidates
+    #     for candi in num_of_candi:
+    #         # and check which candidate the vote was cast for
+    #         if vote == candi:
+    #             # then tally that vote under the candidates slot
+    #             total_votes[num_of_candi.index(candi)] += 1
+    # # returns a list that corresponds with candidate list
+    # return total_votes
 
 
-# calulates percent of votes each candidate got.
+# calulates percent of votes each candidate got returns dictionary
 def percent_vote(all_votes,total_votes):
-    percent_wins = (total_votes / all_votes) * 100
+
+    percent_wins = {}
+    for vote in total_votes:
+        percent_wins[vote] = (int(total_votes[vote]) / int(all_votes)) * 100 
+
+    # percent_wins = (total_votes / all_votes) * 100
 
     return percent_wins
 
@@ -73,12 +86,18 @@ def winner(candidate,total_votes):
     # stores most votes tied to candidate
     winner_total = 0
 
-    # runs thru all candidates
-    for i in range(len(candidate)):
-        # compares all vote counts
-        if total_votes[i] > winner_total:
-            winner_winner = candidate[i]
-            winner_total = total_votes[i]
+    # runs thru dict of all candidates
+    for vote in candidate:
+        if total_votes[vote] > winner_total:
+            winner_winner = vote
+            winner_total = total_votes[vote]
+
+    # # runs thru all candidates
+    # for i in range(len(candidate)):
+    #     # compares all vote counts
+    #     if total_votes[i] > winner_total:
+    #         winner_winner = candidate[i]
+    #         winner_total = total_votes[i]
 
     # returns candidate name (string)
     return winner_winner
@@ -117,24 +136,19 @@ with open(csvpath) as csvfile:
 
 
 # making variable for total votes cast
-# print(f'Total Votes: {count_poll(poll[keys[0]])}')
 total_votes = count_poll(poll[keys[0]])
 
 # making list of all candidates
-# print(candidates(poll[keys[2]]))
-list_of_candidates = candidates(poll[keys[2]])
+dict_of_candidates = candidates(poll[keys[2]])
 
 # making list of total votes for each candidates
-# print(totaling_vote(poll[keys[2]],list_of_candidates))
-list_of_totals = totaling_vote(poll[keys[2]],list_of_candidates) 
+dict_of_totals = totaling_vote(poll[keys[2]]) 
 
 # making list of percentage of total votes each candidate got
-# print(percent_vote(total_votes,list_of_totals))
-list_of_percents = percent_vote(total_votes,list_of_totals)
+dict_of_percents = percent_vote(total_votes,dict_of_totals)
 
 # which candadate won
-# print(winner(list_of_candidates,list_of_totals))
-winner_of_poll = winner(list_of_candidates,list_of_totals)
+winner_of_poll = winner(dict_of_candidates,dict_of_totals)
 
 #---------------------------------------------------------------------------------
 # making a formatted string of for printing and writing
@@ -157,7 +171,7 @@ poll_summary_2 = (
 # print
 
 print(poll_summary_1)
-[print(f'{list_of_candidates[i]}: {list_of_percents[i]:.3f}% ({list_of_totals[i]:.0f})') for i in range(len(list_of_candidates))]
+[print(f'{dict_of_candidates[candi]}: {dict_of_percents[candi]:.3f}% ({dict_of_totals[candi]:.0f})') for candi in dict_of_candidates]
 print(poll_summary_2)
 
 
@@ -166,7 +180,7 @@ print(poll_summary_2)
 polling = open('analysis/poll_summary.txt', 'w')
 polling.write(poll_summary_1)
 polling.write('\n')
-[polling.write(f'{list_of_candidates[i]}: {list_of_percents[i]:.3f}% ({list_of_totals[i]:.0f})\n') for i in range(len(list_of_candidates))]
+[polling.write(f'{dict_of_candidates[candi]}: {dict_of_percents[candi]:.3f}% ({dict_of_totals[candi]:.0f})\n') for candi in dict_of_candidates]
 polling.write(poll_summary_2)
 polling.close()
 
